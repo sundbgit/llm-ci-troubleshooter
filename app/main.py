9e8e701b170ac3d46f2804e6b0edbfb73db342a0
Sunil Poojary <sun.p89@gmail.com>
2026-03-08 12:03:17 +0530

    Update main.py

--- app/main.py
+++ app/main.py
@@ -1,3 +1,21 @@
 # Api layer
 
+from fastapi import FastAPI
+from pydantic import BaseModel
+from app.analyzer import analyze_log
+
+app = FastAPI()
+
+class LogRequest(BaseModel):
+    log: str
+
+@app.post("/analyze_ci_log/")
+def analyze(request: LogRequest):
+
+    result = analyze_log(request.log)
+
+    return {
+        "suggested_fix": result
+    }
+
 
