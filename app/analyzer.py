e323ebdcce1631e6b0f368f16f77b006a817b312
Sunil Poojary <sun.p89@gmail.com>
2026-03-08 12:02:57 +0530

    Update analyzer.py

--- app/analyzer.py
+++ app/analyzer.py
@@ -1,1 +1,25 @@
 # log analysis logic
+
+from app.model_loader import model, tokenizer
+
+def analyze_log(ci_log):
+
+    prompt = f"""
+You are an DevOps expert.
+
+Analyze the CI failure and suggest a fix.
+
+CI Log:
+{ci_log}
+
+Fix:
+"""
+
+    inputs = tokenizer(prompt, return_tensors="pt")
+
+    outputs = model.generate(
+        **inputs,
+        max_new_tokens=60
+    )
+
+    return tokenizer.decode(outputs[0], skip_special_tokens=True)
